db5a9a397e87a497bf0ff92089b90979704bf6ed
peknight <JKpeknight@gmail.com>
2026-01-28 09:25:04 +0800

    增加x64_linux_jdk

--- app-build/shared/src/main/scala/com/peknight/app/build/package.scala
+++ app-build/shared/src/main/scala/com/peknight/app/build/package.scala
@@ -13,14 +13,14 @@ package object build:
       val url: Uri = Uri.unsafeFromString(s"https://github.com/fatedier/frp/releases/download/v$version/frp_${version}_linux_amd64.tar.gz")
     end frp
   end fatedier
-  // https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_x64_linux_hotspot_25.0.2_10.tar.gz
   object adoptium:
     object temurin:
+      // https://github.com/adoptium/temurin25-binaries/releases/
       object jdk:
         object x64:
           object linux:
             val version: String = "25.0.2_10"
-            val url: Uri = Uri.unsafeFromString(s"https://github.com/adoptium/temurin25-binaries/releases/download/${URLEncoder.encode(version.replace('_', '+'), StandardCharsets.UTF_8)}/OpenJDK25U-jdk_x64_linux_hotspot_$version.tar.gz")
+            val url: Uri = Uri.unsafeFromString(s"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-${URLEncoder.encode(version.replace('_', '+'), StandardCharsets.UTF_8)}/OpenJDK25U-jdk_x64_linux_hotspot_$version.tar.gz")
           end linux
         end x64
       end jdk
