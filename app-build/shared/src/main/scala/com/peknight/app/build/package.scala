2bc695d9903b8ae30d03982c6403ad07dc1dc7ea
peknight <JKpeknight@gmail.com>
2026-02-03 09:14:14 +0800

    add fs2.tar and fs2.zip

--- app-build/shared/src/main/scala/com/peknight/app/build/package.scala
+++ app-build/shared/src/main/scala/com/peknight/app/build/package.scala
@@ -9,7 +9,7 @@ package object build:
   object fatedier:
     object frp:
       // https://github.com/fatedier/frp/releases/
-      val version: String = "0.66.0"
+      val version: String = "0.67.0"
       val url: Uri = Uri.unsafeFromString(s"https://github.com/fatedier/frp/releases/download/v$version/frp_${version}_linux_amd64.tar.gz")
     end frp
   end fatedier
